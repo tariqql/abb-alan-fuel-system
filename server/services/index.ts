b46a2f5c9d00344a9644bc04e7a3548bb7937b4e
tariqql <tariq_aldihani@icloud.com>
2026-02-04 09:07:17 +0300

    Add customer verification and KYC system - نظام التحقق من العملاء

---
+++ server/services/index.ts
@@ -0,0 +1,10 @@
+/**
+ * خدمات التحقق والتقييم
+ * Verification & Rating Services
+ */
+
+export { nafathService, NafathService } from "./nafath.service";
+export { complianceService, ComplianceService } from "./compliance.service";
+export { creditService, CreditService } from "./credit.service";
+export { employmentService, EmploymentService } from "./employment.service";
+export { customerRatingService, CustomerRatingService } from "./customer-rating.service";
